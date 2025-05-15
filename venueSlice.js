3df850b731662259e7e6b398371dd83b29698f8e
RonManatee <athompson0103@gmail.com>
2025-05-15 10:47:04 -0400

    Update venueSlice.js

--- venueSlice.js
+++ venueSlice.js
@@ -1,61 +1,59 @@
 // venueSlice.js
-inport {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const venueSlice - createSlice({
+export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
+      img: "",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
+      img: "",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
+      img: "",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
+      img: "",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
+      img: "",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
     },
   
   ],
-  // List of the available locations with their seating and cost numbers
   reducers: {
-   // This function handles incrementing the quantity of a venue item in the state. the state is Audiotrium Hall
+   
     incrementQuantity: (state, action) => {
       const { payload: index } = action;
       if (state[index]) {
         if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
-          return;        } //checks to see if the state is provided with a quantity of 3 or more
-        state[index].quantity++; //otherwise it increases by 1
+          return;        }
+        state[index].quantity++;
       }
-    }, // This function handles decrementing the quantity of a venue item in the state.
+    },
     decrementQuantity: (state, action) => {
       const { payload: index } = action;
-      if (state[index] && state[index].quantity > 0) { //checks if state is provided and greater than 0
-        state[index].quantity--; //if both are met, it will decrease by 1
+      if (state[index] && state[index].quantity > 0) {
+        state[index].quantity--;
       }
     },
   },
 });
 
 export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
-
 export default venueSlice.reducer;
