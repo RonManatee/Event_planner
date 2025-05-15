ada9ba21d6681e79622dcebaadb7d4ff111f9892
RonManatee <athompson0103@gmail.com>
2025-05-15 09:35:19 -0400

    Update store.js

--- store.js
+++ store.js
@@ -1,10 +1,14 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit'; //this creates the Redux from the toolkit
 import venueReducer from './venueSlice'; //contains the reducer 'venueReducer'
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
   },
 });
 // This code creates a global Redux store using the @reduxjs/toolkit\ configureStore() 
